68ee9e327a4b41d547a0e8d2d4ed560f8f255f70
Michael Reese <michaelate@gmail.com>
2024-08-11 11:40:27 +0200

    multiple windows with native C gtk4 api

--- src/graphics_gtk4_native.d
+++ src/graphics_gtk4_native.d
@@ -24,10 +24,15 @@ void g_signal_connect_after(Widget,Callback)(Widget w, const char* signal_name, 
 
 import graphics;
 
+
+
 class Gtk4NativeGui : Gui {
 
 	static GtkApplication* application  = null;
 	static GApplication*   gapplication = null;
+
+	static MainWindow[string] main_windows;
+
 
 	extern(C)
 	static gboolean 
@@ -53,52 +58,20 @@ class Gtk4NativeGui : Gui {
 	activate (GtkApplication *app,
 	          gpointer        user_data)
 	{
-	  GtkWidget *window;
-	  //GtkWidget *frame;
-	  //GtkWidget *drawing_area;
-	  //GtkGesture *drag;
-	  //GtkGesture *press;
-
-	  window = gtk_application_window_new (app);
-	  gtk_window_set_title (cast(GtkWindow*)window, "Drawing Area");
-
-	  //g_signal_connect (window, "destroy", &close_window, null);
-
-	  //frame = gtk_frame_new (null);
-	  //gtk_window_set_child (cast(GtkWindow*)window, frame);
-
-	  //drawing_area = gtk_drawing_area_new ();
-	  ///* set a minimum size */
-	  //gtk_widget_set_size_request (drawing_area, 100, 100);
-
-	  //gtk_frame_set_child (cast(GtkFrame*)frame, drawing_area);
-
-	  //gtk_drawing_area_set_draw_func (cast(GtkDrawingArea*)drawing_area, &draw_cb, null, null);
-
-	  //g_signal_connect_after (drawing_area, "resize", &resize_cb, null);
-
-	  //drag = gtk_gesture_drag_new ();
-	  //gtk_gesture_single_set_button (cast(GtkGestureSingle*)drag, GdkButton.PRIMARY);
-	  //gtk_widget_add_controller (drawing_area, cast(GtkEventController*)drag);
-	  //g_signal_connect (drag, "drag-begin", &drag_begin, drawing_area);
-	  //g_signal_connect (drag, "drag-update", &drag_update, drawing_area);
-	  //g_signal_connect (drag, "drag-end", &drag_end, drawing_area);
-
-	  //press = gtk_gesture_click_new ();
-	  //gtk_gesture_single_set_button (cast(GtkGestureSingle*)press, GdkButton.SECONDARY);
-	  //gtk_widget_add_controller (drawing_area, cast(GtkEventController*)press);
-
-	  //g_signal_connect (press, "pressed", &pressed, drawing_area);
-
+		auto self = cast(Gtk4NativeGui)user_data;
+		import fairy;
+	 
 		immutable ulong refresh_period_ms = 20;
 		g_timeout_add(refresh_period_ms, &timeout_callback, null);
+		foreach(name, ref canvas; session.windows) {
+			self.add_window(name, canvas);
+		}
 
-		gtk_window_present (cast(GtkWindow*)window);
 	}
 
-	//static GtkWindow*[string] main_windows;
 
 	override void add_window(string name, ref CanvasProperties canvas) {
+		main_windows[name] = new MainWindow(name, &canvas, application);
 	}
 	override void close_window(string name) {
 	}
@@ -124,7 +97,7 @@ class Gtk4NativeGui : Gui {
 		application = gtk_application_new ("de.risingedge.fairy", G_APPLICATION_NON_UNIQUE);
 		scope(exit) g_object_unref (application);
 
-		g_signal_connect!(GtkApplication*)(application, "activate".toStringz, &activate, cast(gpointer)this);
+		g_signal_connect!(GtkApplication*)(application, "activate", &activate, cast(gpointer)this);
 
 		int argc = 0;
 		char* argv = null;
@@ -134,3 +107,21 @@ class Gtk4NativeGui : Gui {
 	}
 }
 
+class MainWindow 
+{
+import graphics;
+
+private:
+	GtkWindow*        window;
+	GtkApplication*   application;
+	CanvasProperties* canvas;
+	this (string name, CanvasProperties* canvas_properties, GtkApplication* app)
+	{
+		application = app;
+		window      = cast(GtkWindow*)gtk_application_window_new(app);
+		canvas      = canvas_properties;
+
+		gtk_window_set_title(window, "Drawing Area");
+		gtk_window_present(window);
+	}
+}
