94c3538e10cc3246edba090a120f5a0a0ba51b13
Michael Reese <michaelate@gmail.com>
2025-03-24 19:02:42 +0100

    more flexible colors

---
+++ src/color.d
@@ -0,0 +1,27 @@
+module color;
+@safe:
+
+static void get_rgb(double c, out uint rgb) {
+	// grayscale
+	//uint i = cast(uint)(255*c);
+	//rgb = 0xff000000 | (i<<16) | (i<<8) | (i<<0);
+
+	// bluish color
+	rgb = 0xff000000;
+	if (c>1.0) c = 1.0;
+	if (c<0.0) c = 0.0;
+	c *= 3;
+	if (c < 1.0) { // back to blue
+		rgb |= cast(uint)(0xff*c);
+		return;
+	}
+	rgb = 0xff0000ff;
+	if (c < 2.0)  { // blue to lightblue
+		c -= 1.0; 
+		rgb |=  (cast(uint)(0x0000ff00*c) & 0x0000ff00) ; 
+		return;
+	}
+	rgb = 0xff00ffff; // lightblue to white
+	c -= 2.0;
+	rgb |= cast(uint)(0xff*c)<<16;
+}
