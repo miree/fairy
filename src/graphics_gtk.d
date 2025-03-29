e74c5e361d9ec301c3e3ac8de5788cdd4734d3dd
Michael Reese <michaelate@gmail.com>
2025-03-30 00:15:03 +0100

    add window frame for gtk3

--- src/graphics_gtk.d
+++ src/graphics_gtk.d
@@ -197,6 +197,7 @@ private:
 	import gtk.ScrolledWindow;
 	import gtk.PopoverMenu;
 	import gtk.Popover;
+	import gdk.Cursor;
 	//version(gtk3) {
 		import gio.Menu;
 		import gio.MenuItem;
@@ -213,7 +214,11 @@ private:
 	static int windowIndexCtr;
 	string name;
 
+	Frame window_toplevel_frame_outer;
+	const int border_size = 3;
 	Frame window_toplevel_frame;
+	Cursor cursor_normal;
+	Cursor cursor_window_resize_top_left;
 	Box window_toplevel_box;
 	HeaderBar header_bar;
 		Button open_menu;
@@ -266,6 +271,10 @@ public:
 		version(gtk3) {
 			move(canvas.xpos, canvas.ypos);
 		}
+
+		cursor_normal = new Cursor(CursorType.ARROW);
+		cursor_window_resize_top_left = new Cursor(CursorType.TOP_LEFT_CORNER);
+
 
 		import ui;
 
@@ -427,7 +436,7 @@ public:
 		header_title = new Label("fairy - " ~ window_name);
 		version(gtk4) { 
 			header_bar.setTitleWidget(header_title); 
-			header_bar.setShowTitleButtons(false); // to match gtk3 behavior
+			//header_bar.setShowTitleButtons(false); // to match gtk3 behavior
 		}
 		version(gtk3) { 
 			header_bar.setTitle("fairy - " ~ window_name); 
@@ -441,7 +450,8 @@ public:
 			auto open_image = new Image;
 			open_image.setFromIconName("open-menu-symbolic", IconSize.LARGE_TOOLBAR);
 			open_menu.setImage(open_image);
-			header_bar.setShowCloseButton(true);		
+			header_bar.setShowCloseButton(true);
+			header_bar.setDecorationLayout("menu:minimize,maximize,close");
 		}
 		version(gtk4) { 
 			open_menu.setIconName("open-menu-symbolic"); 
@@ -479,7 +489,56 @@ public:
 		//});
 		//header_bar.packStart(open_soundscope);
 
+		window_toplevel_frame_outer = new Frame(cast(string)null);
 		window_toplevel_frame = new Frame(cast(string)null);
+		version(gtk3) {
+			//window_toplevel_frame_outer.addOnMotionNotify(delegate bool(GdkEventMotion *event_motion, Widget w){
+			//		GdkRectangle rect;
+			//		window_toplevel_frame_outer.getAllocation(rect);
+
+			//		double x = event_motion.x;
+			//		double y = event_motion.y;
+			//		import std.stdio;
+			//		writeln("rect:", rect.x, ",", rect.y, ",", rect.width, ",", rect.height, "   ", x,",",y, "  ", w is window_toplevel_frame_outer);					
+			//		//bool ctrl  = (event_motion.state & GdkModifierType.CONTROL_MASK) != 0;
+			//		//bool shift = (event_motion.state & GdkModifierType.SHIFT_MASK  ) != 0;
+			//		//painter.mouse_motion(x,y, cast(PlotArea)w, ctrl, shift);
+			//		if (x >= border_size && x < rect.width-border_size &&
+			//			y >= border_size && y < rect.height-border_size) {
+			//			//setCursor(new Cursor(CursorType.ARROW));
+			//			setCursor(cursor_normal);
+			//		} else {
+			//			setCursor(cursor_window_resize_top_left);
+			//		}
+			//		return false;
+			//	});
+			//window_toplevel_frame_outer.addOnButtonPress(delegate bool(GdkEventButton *event_button, Widget w) {
+			//		import gdk.Event;
+			//		int nPress = Event.isDoubleClick(event_button)?2:1;
+			//		PlotArea plot_area = cast(PlotArea)w;
+			//		double x = event_button.x, y = event_button.y;
+			//		bool ctrl  = (event_button.state & GdkModifierType.CONTROL_MASK) != 0;
+			//		bool shift = (event_button.state & GdkModifierType.SHIFT_MASK  ) != 0;
+			//		if (event_button.button == 1) painter.left_button_pressed (nPress,x,y,this,ctrl,shift);
+			//		if (event_button.button == 2) painter.mid_button_pressed  (nPress,x,y,this,ctrl,shift);		
+			//		if (event_button.button == 3) painter.right_button_pressed(nPress,x,y,ctrl,shift);
+			//		return false;
+			//	});
+			//window_toplevel_frame_outer.addOnButtonRelease(delegate bool(GdkEventButton *event_button, Widget w) {
+			//		import gdk.Event;
+			//		int nPress = Event.isDoubleClick(event_button)?2:1;
+			//		PlotArea plot_area = cast(PlotArea)w;
+			//		double x = event_button.x, y = event_button.y;
+			//		bool ctrl  = (event_button.state & GdkModifierType.CONTROL_MASK) != 0;
+			//		bool shift = (event_button.state & GdkModifierType.SHIFT_MASK  ) != 0;
+			//		if (event_button.button == 1) painter.left_button_released (nPress,x,y,ctrl,shift);
+			//		if (event_button.button == 2) painter.mid_button_released  (nPress,x,y,ctrl,shift);		
+			//		if (event_button.button == 3) painter.right_button_released(nPress,x,y,ctrl,shift);
+			//		return false;
+			//	});
+		}
+
+
 		window_toplevel_box = new Box(GtkOrientation.VERTICAL,0);
 
 
@@ -580,7 +639,11 @@ public:
 		window_toplevel_box.append(header_bar);
 		window_toplevel_box.append(workspace);
 		window_toplevel_frame.setChild(window_toplevel_box);
-		this.setChild(window_toplevel_frame);
+		version(gtk3) {
+			window_toplevel_frame.setBorderWidth(border_size);
+		}
+		window_toplevel_frame_outer.setChild(window_toplevel_frame);
+		this.setChild(window_toplevel_frame_outer);
 
 		setSizeRequest(-1,-1);
 		present();
